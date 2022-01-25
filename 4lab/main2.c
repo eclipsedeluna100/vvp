93070b5359c8a4b280cee9902775fb13d2d77e3d
eclipsedeluna100 <78378281+eclipsedeluna100@users.noreply.github.com>
2022-01-26 02:43:28 +0300

    Delete 4lab directory

--- 4lab/main2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    float pi,d,l;
-    pi=3.14;
-    scanf("%f", &d);
-    l=pi*d;
-    printf("%f", l);
-}
--- 4lab/main3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    float a,b,sr;
-    scanf("%f", &a);
-    scanf("%f", &b);
-    sr=(a+b)/2;
-    printf("%f", sr);
-}
