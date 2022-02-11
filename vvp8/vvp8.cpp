4e16e6bc9caa7f58a58e762a74e0131380e40a7b
eclipsedeluna100 <78378281+eclipsedeluna100@users.noreply.github.com>
2022-02-12 01:26:09 +0300

    Add files via upload

---
+++ vvp8/vvp8.cpp
@@ -0,0 +1,38 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <clocale>
+#include <cmath>
+
+
+using namespace std;
+
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	int x;
+	cout << "Байты: ";
+	cin >> x;
+	cout << "Количество полных килобайтов: " << x / 1024 << endl;
+
+
+	int a, b;
+	cout << "A, B: ";
+	cin >> a >> b;
+	cout << "Количество отрезков B, размещенных на отрезке А: " << a / b << endl;
+
+	cout << "A, B: ";
+	cin >> a >> b;
+	cout << "Длина незанятой части отрезка А: " << a - b * (a / b) << endl;
+
+
+	cout << "Двузначное число: ";
+	cin >> x;
+	cout << "Двузначное число после перестановки: " << x % 10 << x / 10 << endl;
+
+	
+	cout << "Трёхзначное число: ";
+	cin >> x;
+	cout << "Полученное число: " << x % 100 << x / 100 << endl;
+}
